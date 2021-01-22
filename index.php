3c0f65c33bb82b9802773e6ab9e59a1cf69b2fd0
Anamika19972831 <75614874+Anamika19972831@users.noreply.github.com>
2021-01-22 16:41:57 +0530

    Forum Project

--- index.php
+++ index.php
@@ -1,36 +1,3 @@
-<?php
-//INSERT INTO `notes` (`sno`, `title`, `description`, `tstamp`) 
-//VALUES (NULL, 'Fruits', 'eat fruits everyday', current_timestamp());
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "notes";
-$conn = mysqli_connect($servername, $username, $password, $database);
-if(!$conn){
-    die("Sorry! Unable to connect with database.<br>");
-  }
-  if ($_SERVER['REQUEST_METHOD']=='POST'){
-    $title = $_POST['title'];
-    $description = $_POST['description'];
-    $sql = "INSERT into notes ( title, description) 
-    values ('$title', '$description')";
-    $result = mysqli_query($conn, $sql);
-    if($result){
-      echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
-      <strong>Success!</strong> Your Email  and  Password has been submitted successfully.
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>';
-
-    }
-    else {
-        echo "Data was not inserted successfully..<br>" . myssqli_error($conn);
-    }
-
-  }
-?>
 <!doctype html>
 <html lang="en">
 
@@ -42,136 +9,75 @@ if(!$conn){
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
         integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <link rel="stylesheet" href="//cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" class="css">
 
-
-    <title>PHP CRUD</title>
-   
+    <title>Welcome to iForum</title>
 </head>
 
 <body>
-    <!-- Button trigger modal -->
-    <!-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#editModal">
-      Edit Modal
-    </button> -->
-
-    <!-- Modal -->
-    <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModal" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="editModal">Edit Note</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                <div class="container mt-4">
-                <form action="/CRUD/index.php" method="POST">
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Note Title</label>
-                        <input type="title" class="form-control" id="titleedit" name="title" aria-describedby="emailHelp">
-                    </div>
-                    <div class="form-group">
-                        <label for="description">Note Description</label>
-                        <textarea class="form-control" id="descriptionedit" name="description" rows="3"></textarea>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Add Note</button>
-                </form>
-                </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
+    <?php include 'Partial/header.php'; ?>
+    <?php include 'Partial/dbconnect.php'; ?>
+    <!--------------slider start------------------------------------->
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="img/slider1.jpg" class="d-block w-100" alt="1">
+            </div>
+            <div class="carousel-item">
+                <img src="img/slider2.jpg" class="d-block w-100" alt="2">
+            </div>
+            <div class="carousel-item">
+                <img src="img/slider3.jpg" class="d-block w-100" alt="3">
             </div>
         </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
-    <!---------------modal end-------------------------------->
-    <!-----------------navbar start------------------>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">PHP CRUD</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    <!----------------------------slider end----------------------------->
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact Us</a>
-                </li>
-
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-        </div>
-    </nav>
-    <!------------------------------------navbar end--------->
-    <!---------------------------------------------from start----------------------------------->
-    <div class="container mt-4">
-        <form action="/CRUD/index.php" method="POST">
-            <div class="form-group">
-                <label for="exampleInputEmail1">Note Title</label>
-                <input type="title" class="form-control" id="title" name="title" aria-describedby="emailHelp">
-            </div>
-            <div class="form-group">
-                <label for="description">Note Description</label>
-                <textarea class="form-control" id="description" name="description" rows="3"></textarea>
-            </div>
-            <button type="submit" class="btn btn-primary">Add Note</button>
-        </form>
-    </div>
-    <!----------------------------form end------------------------->
-    <!------------------------next div start----------------------->
     <div class="container my-4">
+        <h2 class="text-center">iForum - Browse Category</h2>
 
-        <table class="table" id="myTable">
-            <thead>
-                <tr>
-                    <th scope="col">SNo</th>
-                    <th scope="col">Title</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-
-  <?php
-     $sql = "SELECT *FROM notes";
-     $result = mysqli_query($conn, $sql);
-     $sno = 0;
-     while($row= mysqli_fetch_assoc($result))
-     {
-      $sno = $sno +1;
-      echo "<tr>
-      <th scope='row'> $sno </th>
-      <td>" . $row['title'] . "</td>
-      <td>" . $row['description'] . "</td>
-      <td><button class='edit btn btn-sm btn-primary' onclick='#editModal'>Edit</button> <button class='btn btn-sm btn-danger'>Delete</button></td>
-      </tr>";
-     
+        <div class="row">
+        <!-- fetch data from database -->
+          <!-- use a for loop to iterate through categories.. -->
+        <?php
+            $sql = "SELECT *FROM category";
+            $result = mysqli_query($conn, $sql);
+             while($row = mysqli_fetch_assoc($result)){
+                $id = $row['category_id'];
+                $cat = $row['category_name'];
+                $desc = $row['category_description'];
+                echo '<div class="col md-4 my-2">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://source.unsplash.com//500x400/?' . $cat . ',php" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"><a href="threadlist.php?catid=' . $id . '">' . $cat . '</a></h5>
+                        <p class="card-text">' . substr($desc, 0, 200 ).'</p>
+                        <a href="threadlist.php?catid=' . $id . '" class="btn btn-primary">View Thread</a>
+                    </div>
+                </div>
+            </div>';
+            }
+        
+        ?>
       
-      
-     }
-  ?>
+           
 
-
-
-        </tbody>
-        </table>
-        <hr>
+        </div>
     </div>
+    <?php include 'Partial/footer.php'?>
 
-    <!------------------------next div end--------------------------->
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
@@ -182,27 +88,6 @@ if(!$conn){
     </script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
         integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
-    </script>
-    <script src="//cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
-    <script>
-    $(document).ready(function() {
-        $('#myTable').DataTable();
-    });
-    </script>
-     <script>
-      edits = getElementsByClassName('edit');
-      Array.from(edits).foreach(element()->{
-        element.addEventListner("click", (e)->{
-          console.log("edit", );
-          tr =  e.target.parentNode.parentNode;
-          title = tr.getElementsByTagName("td")[0].innerText;
-          description = tr.getElementsByTagName("td")[1].innerText;
-          console.log(title, description);
-          titleedit.value = title;
-          descriptionedit.value = description;
-          $("#editModal").modal('toggle');
-        })
-      })
     </script>
 </body>
 
